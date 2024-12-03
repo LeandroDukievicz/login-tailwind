b1b1134eaab752dfde507d8c3fe975df3750fa2b
LeandroDukievicz <ldukie@icloud.com>
2024-12-02 23:03:46 -0300

    📦 FEAT: I started create a login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textlogin: "#4682B4", // Define the custom color here
+      },
+    },
   },
   plugins: [],
 };
